f528187111ec421c0885716dcb9977685e36232f
Melissa Gomez <melissagomez3024@gmail.com>
2026-03-27 18:30:55 +0100

    Initial commit

---
+++ src/main/java/com/videoclub/videoclub_api/VideoclubApiApplication.java
@@ -0,0 +1,14 @@
+package com.videoclub.videoclub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubApiApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(VideoclubApiApplication.class, args);
+		System.out.println("Hello World - El sistema funciona");
+	}
+
+}
---
+++ src/test/java/com/videoclub/videoclub_api/VideoclubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoclub.videoclub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoclubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
